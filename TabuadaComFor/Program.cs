228f0bbe24b826a2b29cab1b541a3ac9f2564aa9
rafael jeferson <rafajeferson@hotmail.com>
2019-09-22 22:18:51 -0300

    Adicionar arquivos de projeto.

---
+++ TabuadaComFor/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace TabuadaComFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1;
+            Console.WriteLine("Digite o número que você quer para a tabuada");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            for (int multiplicador = 0; multiplicador <= 10; multiplicador++)
+            {
+                Console.WriteLine($"{n1} X {multiplicador} = {n1*multiplicador} ");
+
+            }
+        }
+    }
+}
